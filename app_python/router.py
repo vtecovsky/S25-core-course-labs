ff3bc368c6f6100770831373d47b72b8e402651a
Saveliy Lekhtin <s.lekhtin@innopolis.university>
2025-01-29 21:32:38 +0300

    feat: add schema

--- app_python/router.py
+++ app_python/router.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter
+from schema import GetTimeResponse
 import datetime
 import pytz
 
 router = APIRouter()
 
 @router.get("/msc_time")
-async def get_moscow_time() -> dict[str, str]:
+async def get_moscow_time() -> GetTimeResponse:
     moscow_tz = pytz.timezone("Europe/Moscow")
     moscow_time = datetime.datetime.now(tz=moscow_tz)
-    return {
-        "current_time_in_moscow": moscow_time.strftime("%Y-%m-%d %H:%M:%S")
-    }
+    return GetTimeResponse(current_time=moscow_time.strftime("%Y-%m-%d %H:%M:%S"))
