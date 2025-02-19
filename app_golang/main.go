77f80d3997349c8df534ed62a6d1afe5ae3fa626
Saveliy Lekhtin <s.lekhtin@innopolis.university>
2025-02-19 10:38:16 +0300

    export metrics

--- app_golang/main.go
+++ app_golang/main.go
@@ -4,9 +4,36 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var (
+	requestsTotal = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+	)
+	requestDuration = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Histogram of response time for /msc_time",
+			Buckets: prometheus.DefBuckets,
+		},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(requestsTotal)
+	prometheus.MustRegister(requestDuration)
+}
+
 func GetMoscowTime(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	requestsTotal.Inc()
+
 	loc, err := time.LoadLocation("Europe/Moscow")
 	if err != nil {
 		http.Error(w, "Unable to load Moscow timezone", http.StatusInternalServerError)
@@ -14,15 +41,18 @@ func GetMoscowTime(w http.ResponseWriter, r *http.Request) {
 	}
 	moTime := time.Now().In(loc)
 	fmt.Fprintf(w, `{"current_time_in_moscow": "%s"}`, moTime)
+
+	duration := time.Since(start).Seconds()
+	requestDuration.Observe(duration)
 }
 
 func main() {
 	http.HandleFunc("/msc_time", GetMoscowTime)
+	http.Handle("/metrics", promhttp.Handler())
 
-	// Start the server on port 8080
 	fmt.Println("Starting server on :8080...")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-	    panic("error starting http server")
+		panic("error starting http server")
 	}
 }
